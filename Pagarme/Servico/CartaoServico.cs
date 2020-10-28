5c0542e5bf28e057e1d4710b0571a78512b180be
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2020-10-28 00:14:39 -0300

    Ajuste id cliente

--- Pagarme/Servico/CartaoServico.cs
+++ Pagarme/Servico/CartaoServico.cs
@@ -24,19 +24,23 @@ namespace Pagarme.Servico
             cartaoDto.Cliente = new ClienteDTO();
             cartaoDto.Cliente.Nome = pessoa.Nome;
             cartaoDto.Cliente.Pais = "br";
-            cartaoDto.Cliente.Id = "541";
+            cartaoDto.Cliente.Id = Guid.NewGuid().ToString();
             cartaoDto.Cliente.Email = "nome@email.com.br";
             cartaoDto.Cliente.Telefones = new List<string> { "+5547988441122" };
             cartaoDto.Cliente.Nascimento = DateTime.Now.AddDays(-28).ToString("yyyy-MM-dd");
             cartaoDto.Cliente.Documentos = new List<DocumentoDTO> { new DocumentoDTO { Numero = pessoa.Documento, Tipo = "cpf" } };
-            cartaoDto.Faturamento = new FaturamentoDTO {Nome = "Nome Completo", Endereco = new EnderecoDTO {CEP = "06714360", Cidade = "Cotia", Estado = "sp", Logradouro = "Rua", Numero = "99", Pais = "br" } };
-            cartaoDto.Itens = new List<ItemDTO> { new ItemDTO {Fisico= true, Id = "12", Nome = "Folha", Quantidade = 1, Valor = 200 } };
+            cartaoDto.Faturamento = new FaturamentoDTO { Nome = "Nome Completo", Endereco = new EnderecoDTO { CEP = "06714360", Cidade = "Cotia", Estado = "sp", Logradouro = "Rua", Numero = "99", Pais = "br" } };
+            cartaoDto.Itens = new List<ItemDTO> { new ItemDTO { Fisico = true, Id = "12", Nome = "Folha", Quantidade = 1, Valor = 200 } };
 
 
             using (var requisicao = new HttpClient())
             {
+                var configuracao = new JsonSerializerSettings();
+                configuracao.NullValueHandling = NullValueHandling.Ignore;
+                configuracao.DefaultValueHandling = DefaultValueHandling.Ignore;
+
                 requisicao.BaseAddress = new Uri(Constante.UrlBase);
-                var conteudo = new StringContent(JsonConvert.SerializeObject(cartaoDto), Encoding.UTF8, MediaTypeNames.Application.Json);
+                var conteudo = new StringContent(JsonConvert.SerializeObject(cartaoDto, configuracao), Encoding.UTF8, MediaTypeNames.Application.Json);
                 var resultado = requisicao.PostAsync(Constante.Transacao, conteudo).Result;
                 if (!resultado.IsSuccessStatusCode)
                 {
